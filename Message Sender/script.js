28a98934938ccc0dcfab0cdd5c3fa138fbaf7701
ogbeche77 <59567201+ogbeche77@users.noreply.github.com>
2020-01-31 12:32:43 +0100

    Add files via upload

---
+++ Message Sender/script.js
@@ -0,0 +1,11 @@
+// mouse click event handler
+var imagess = document.getElementsByTagName ("img");
+for (var i = 0; i < imagess.length; i++) {
+    imagess [i].onclick = showAnswer;
+}
+function showAnswer(eventObj) {
+    var image = eventObj.target;
+    var name = image.id;
+    name = name + ".jpg";
+    image.src =name;
+}
