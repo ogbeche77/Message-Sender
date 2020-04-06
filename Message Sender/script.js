d5ce37c540a7edbc5ebd668777188abeb1f5acb3
ogbeche77 <ogbeche77@yahoo.com>
2020-04-06 11:11:03 +0200

    btn

--- Message Sender/script.js
+++ Message Sender/script.js
@@ -1,15 +1,15 @@
-var buttonSender=document.querySelector("#buttonSend");
-var messageOne=document.querySelector("#messageInput");
-var messageTwo=document.querySelector("#messageOutput");
+var buttonSender = document.querySelector("#buttonSend");
+var messageOne = document.querySelector("#messageInput");
+var messageTwo = document.querySelector("#messageOutput");
 
-buttonSender.addEventListener("click", messageSender)
+buttonSender.addEventListener("click", messageSender);
 
-function messageSender(){
-    let content = messageOne.value;
-    if(content === ""){
-        alert("Enter Valid Value")
-    } else{
-    }
-    messageTwo.innerHTML = content;
-    messageOne.value = "";
+function messageSender() {
+  let content = messageOne.value;
+  if (content === "") {
+    alert("Enter Valid Value");
+  } else {
+  }
+  messageTwo.innerHTML = content;
+  messageOne.value = "";
 }
